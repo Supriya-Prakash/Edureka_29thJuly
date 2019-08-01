12733e6e7e0bafaf22a1a0a8c2abfbbc1bca4473
Supriya <supriyaprakash199@gmail.com>
2019-08-01 12:23:10 -0400

    first commit

---
+++ src/Module1/JavaFirstClass.java
@@ -0,0 +1,10 @@
+package Module1;
+
+public class JavaFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Module2/BasicSeleniumCommands.java
@@ -0,0 +1,10 @@
+package Module2;
+
+public class BasicSeleniumCommands {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
